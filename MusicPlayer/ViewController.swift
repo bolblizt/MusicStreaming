6a1f1ceb84b444f2bfc5f8f3effaf820445ee2ff
Dominic Rivera <dominicrivera@Dominics-MacBook-Pro.local>
2017-07-01 11:44:33 +1000

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Dominic Edwayne Rivera on 1/7/17.
+//  Copyright © 2017 Dominic Edwayne Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
