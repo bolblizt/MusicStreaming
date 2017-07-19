99f996c1ccfc9714e5cd22bb99126824c053ff8d
Dominic Rivera <dominic.edwayne.rivera@gmail.com>
2017-07-19 12:47:58 +1000

    First Commit

---
+++ MusicPlayer/Music.swift
@@ -0,0 +1,9 @@
+//
+//  Music.swift
+//  MusicPlayer
+//
+//  Created by Dominic Edwayne Rivera on 1/7/17.
+//  Copyright © 2017 Dominic Edwayne Rivera. All rights reserved.
+//
+
+import Foundation
---
+++ MusicPlayer/SongsTableView.swift
@@ -0,0 +1,21 @@
+//
+//  SongsTableView.swift
+//  MusicPlayer
+//
+//  Created by Dominic Edwayne Rivera on 19/7/17.
+//  Copyright © 2017 Dominic Edwayne Rivera. All rights reserved.
+//
+
+import UIKit
+
+class SongsTableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
